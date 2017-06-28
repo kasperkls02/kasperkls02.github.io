385f6edde7ba3f2d3be848c6b0d377c39eb7690f
kasperkls02 <kasperkls02@gmail.com>
2017-06-28 17:38:23 +0200

    Updated scripts

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function Home() {
     $("#include").load("pages/Home.htm");
 }
+
 function About() {
     $("#include").load("pages/About.htm");
 }
+
 function randomnote() {
     $("#include").load("music/random-note.html");
 }
