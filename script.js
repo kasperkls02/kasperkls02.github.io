9a5656357d2f30bfcda4899bd08c4d533b8b0a9b
kasperkls02 <kasperkls02@gmail.com>
2017-06-28 17:36:59 +0200

    asd

--- script.js
+++ script.js
@@ -6,7 +6,5 @@ function About() {
     $("#include").load("pages/About.htm");
 }
 function randomnote() {
-    document.getElementById('include').innerHTML = "Halløj";
     $("#include").load("music/random-note.html");
-    document.getElementById('include').innerHTML = "Halløj";
 }
