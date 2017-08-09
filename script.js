9a5523a51f431e7f7472ae5938503c103f6692d9
Kasper <kasperkls02@gmail.com>
2017-08-09 20:17:12 +0200

    sa

--- script.js
+++ script.js
@@ -14,9 +14,15 @@ function beregnere() {
     $("#include").load("beregnere/beregnere.html");
 }
 
+function addition_site() {
+    $("#include").load("beregnere/math/addition.html");
+}
+
+
+
 calculate = function()
 {
     var resources = document.getElementById('a1').value;
-    var minutes = document.getElementById('a2').value; 
-    document.getElementById('a3').value = parseInt(resources)*parseInt(minutes);
+    var minutes = document.getElementById('a2').value;
+    document.getElementById('a3').value = parseInt(resources)+parseInt(minutes);
 }
